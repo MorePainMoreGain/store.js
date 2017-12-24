41f19c7d82918aa132825c3eb63dbc5cd707a2a3
dosir <517441830@qq.com>
2017-12-24 15:10:41 +0800

    存取操作添加json处理，和encode转码

--- store.js
+++ store.js
@@ -5,43 +5,86 @@
  * */
 
 var store = (function () {
-    var api               = {},
-        win               = window,
-        doc               = win.document,
-        localStorageName  = 'localStorage',
+    var api = {},
+        win = window,
+        doc = win.document,
+        localStorageName = 'localStorage',
         globalStorageName = 'globalStorage',
-        sessionStorageName= 'sessionStorage',
+        sessionStorageName = 'sessionStorage',
         storage;
 
-    api.set    = function (key, value) {};
-    api.get    = function (key)        {};
-    api.remove = function (key)        {};
-    api.clear  = function ()           {};
+    api.set = function (key, value) {};
+    api.get = function (key) {};
+    api.remove = function (key) {};
+    api.clear = function () {};
 
-    if(sessionStorageName in win && win[sessionStorageName]){
-        storage    = win[sessionStorageName];
-        api.set    = function (key, val) {storage.setItem(key, val)};
-        api.get    = function (key)      {return storage.getItem(key)};
-        api.remove = function (key)      {storage.removeItem(key)};
-        api.clear  = function()          {storage.clear()};
+    function setVal(val) {
+        var vall = JSON.stringify(val);
+        return encodeURIComponent(vall);
+    }
+
+    function getVal(val) {
+        var vall = decodeURIComponent(val);
+        return JSON.parse(vall);
+    }
+    if (sessionStorageName in win && win[sessionStorageName]) {
+        storage = win[sessionStorageName];
+        api.set = function (key, val) {
+            var vall = setVal(val);
+            storage.setItem(key, vall)
+        };
+        api.get = function (key) {
+            var vall = storage.getItem(key);
+            return getVal(vall);
+        };
+        api.remove = function (key) {
+            storage.removeItem(key)
+        };
+        api.clear = function () {
+            storage.clear()
+        };
 
     } else if (localStorageName in win && win[localStorageName]) {
-        storage    = win[localStorageName];
-        api.set    = function (key, val) { storage.setItem(key, val) };
-        api.get    = function (key)      { return storage.getItem(key) };
-        api.remove = function (key)      { storage.removeItem(key) };
-        api.clear  = function ()         { storage.clear() };
+        storage = win[localStorageName];
+        api.set = function (key, val) {
+            var vall = setVal(val);
+            storage.setItem(key, vall)
+        };
+        api.get = function (key) {
+            var vall = storage.getItem(key);
+            return getVal(vall);
+        };
+        api.remove = function (key) {
+            storage.removeItem(key)
+        };
+        api.clear = function () {
+            storage.clear()
+        };
 
     } else if (globalStorageName in win && win[globalStorageName]) {
-        storage    = win[globalStorageName][win.location.hostname];
-        api.set    = function (key, val) { storage[key] = val };
-        api.get    = function (key)      { return storage[key] && storage[key].value };
-        api.remove = function (key)      { delete storage[key] };
-        api.clear  = function ()         { for (var key in storage ) { delete storage[key] } };
+        storage = win[globalStorageName][win.location.hostname];
+        api.set = function (key, val) {
+            var vall = setVal(val);
+            storage[key] = vall
+        };
+        api.get = function (key) {
+            var vall = storage[key] && storage[key].value;
+            return getVal(vall)
+        };
+        api.remove = function (key) {
+            delete storage[key]
+        };
+        api.clear = function () {
+            for (var key in storage) {
+                delete storage[key]
+            }
+        };
 
     } else if (doc.documentElement.addBehavior) {
         function getStorage() {
-            if (storage) { return storage }
+            if (storage) {
+                return storage
+            }
             storage = doc.body.appendChild(doc.createElement('div'));
             storage.style.display = 'none';
             // See http://msdn.microsoft.com/en-us/library/ms531081(v=VS.85).aspx
@@ -52,12 +95,14 @@ var store = (function () {
         }
         api.set = function (key, val) {
             var storage = getStorage();
-            storage.setAttribute(key, val);
+            var vall = setVal(val);
+            storage.setAttribute(key, vall);
             storage.save(localStorageName);
         };
         api.get = function (key) {
             var storage = getStorage();
-            return storage.getAttribute(key);
+            var vall = storage.getAttribute(key);
+            return getVal(vall);;
         };
         api.remove = function (key) {
             var storage = getStorage();
@@ -68,7 +113,7 @@ var store = (function () {
             var storage = getStorage();
             var attributes = storage.XMLDocument.documentElement.attributes;
             storage.load(localStorageName);
-            for (var i=0, attr; attr = attributes[i]; i++) {
+            for (var i = 0, attr; attr = attributes[i]; i++) {
                 storage.removeAttribute(attr.name);
             }
             storage.save(localStorageName);
